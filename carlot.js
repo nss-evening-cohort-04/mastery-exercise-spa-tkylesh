187ccde0535b7e7fcfa7c462bf3f607e175f2870
Taylor Shuff <tkylesh@gmail.com>
2016-09-12 14:47:22 -0500

    inital commit of spa quiz.  HTML page fully wired up.

---
+++ carlot.js
@@ -0,0 +1,14 @@
+var CarLot = (function () {
+  var inventory = [];
+
+  return {
+    loadInventory: function (callback) {
+      var inventoryLoader = new XMLHttpRequest();
+
+      inventoryLoader.addEventListener("load", function () {
+
+      });
+    }
+  };
+
+})();
